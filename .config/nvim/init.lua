3f8f31a2fe2510c811d23675688bec44ce315fd1
SeakMengs <hseakmeng22@gmail.com>
2024-08-13 13:09:20 +0700

    Lazyvim: default copilot by default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Disable copilot by default, use ":Copilot enable" to enable when needed
+vim.cmd(":Copilot disable")
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<cr>", { desc = "Toggle NeoTree" })
-vim.keymap.set("n", "<leader>ac", "<cmd>Copilot toggle<cr>", { desc = "Toggle (Copilot)" })
+vim.keymap.set("n", "<leader>ae", "<cmd>Copilot enable<cr>", { desc = " Enable (Copilot)" })
+vim.keymap.set("n", "<leader>ac", "<cmd>Copilot disable<cr>", { desc = " Disable (Copilot)" })
