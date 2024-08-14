16955ec822f8fcb70911acb5c4b604596f08ea56
SeakMengs <hseakmeng22@gmail.com>
2024-08-14 17:01:22 +0700

    Lazyvim: Add oil.nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<cr>", { desc = "Toggle NeoTree" })
-vim.keymap.set("n", "<leader>ae", "<cmd>Copilot enable<cr>", { desc = " Enable (Copilot)" })
-vim.keymap.set("n", "<leader>ac", "<cmd>Copilot disable<cr>", { desc = " Disable (Copilot)" })
+
+local keymap = vim.api.nvim_set_keymap
+
+-- Ctrl+n is set to oil.nvim instead of neotree
+-- vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<cr>", { desc = "Toggle NeoTree" })
+
+keymap("n", "<leader>ae", "<cmd>Copilot enable<cr>", { desc = " Enable (Copilot)" })
+keymap("n", "<leader>ac", "<cmd>Copilot disable<cr>", { desc = " Disable (Copilot)" })
+
+-- Save the file with Ctrl+S in insert mode then exit to normal mode
+keymap("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,78 @@
+local function max_height()
+  local height = vim.fn.winheight(0)
+  if height >= 40 then
+    return 30
+  elseif height >= 30 then
+    return 20
+  else
+    return 10
+  end
+end
+
+return {
+  -- Disable neo-tree and use oil.nvim instead, simple and fast
+  { "nvim-neo-tree/neo-tree.nvim", enabled = true },
+  {
+    "stevearc/oil.nvim",
+    cmd = "Oil",
+    commit = "18272aba9d00a3176a5443d50dbb4464acc167bd",
+    opts = {
+      -- Set to false if you still want to use netrw.
+      default_file_explorer = true,
+      -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+      delete_to_trash = true,
+      -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+      skip_confirm_for_simple_edits = true,
+      -- Set to false to disable all of the above keymaps
+      use_default_keymaps = true,
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name)
+          local ignore_folders = { "node_modules", "dist", "build", "coverage", "__pycache__" }
+          return vim.startswith(name, ".") or vim.tbl_contains(ignore_folders, name)
+        end,
+      },
+      -- Configuration for the floating window in oil.open_float
+      float = {
+        padding = 2,
+        max_width = 120,
+        max_height = max_height(),
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      -- Custom Keymap
+      keymaps = {
+        ["<C-c>"] = false,
+        ["<C-s>"] = "actions.save",
+        ["q"] = "actions.close",
+        -- ["<Esc>"] = "actions.close",
+        ["<C-y>"] = "actions.copy_entry_path",
+      },
+    },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- Use g? to see default key mappings
+    keys = {
+      {
+        "<C-n>",
+        function()
+          local oil = require("oil")
+          oil.toggle_float()
+
+          -- -- Wait until oil has opened, for a maximum of 1 second.
+          vim.wait(1000, function()
+            return oil.get_cursor_entry() ~= nil
+          end)
+          if oil.get_cursor_entry() then
+            oil.open_preview()
+          end
+        end,
+        desc = "Open file explorer (Oil)",
+      },
+    },
+  },
+}
