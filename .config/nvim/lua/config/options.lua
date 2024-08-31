165e080b67202c8bd7edd3f909031d8904d566b0
SeakMengs <hseakmeng22@gmail.com>
2024-08-31 20:28:49 +0700

    Add japanese home wallpaper. Update package in neovim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,8 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.winbar = "%=%m %f"
-vim.opt.relativenumber = false
+local opt = vim.opt
+
+-- Appearance
+opt.winbar = "%=%m %f"
+opt.number = true
+opt.relativenumber = false
 
 -- Auto format on save
 vim.g.autoformat = true
+
+-- Tab / Indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
--- .config/nvim/lua/plugins/autotag.lua
+++ .config/nvim/lua/plugins/autotag.lua
@@ -1,7 +1,7 @@
 return {
   {
     "windwp/nvim-ts-autotag",
-    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact", "html" },
+    -- ft = { "javascript", "javascriptreact", "typescript", "typescriptreact", "html" },
     config = function()
       require("nvim-ts-autotag").setup()
     end,
