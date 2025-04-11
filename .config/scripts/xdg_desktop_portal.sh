0154a13fd8ad5722e56ece236d1606c2bc3a1741
SeakMengs <hseakmeng22@gmail.com>
2025-04-11 17:27:54 +0700

    Change hyprland window rule follow the recent update and bump lazyvim dependencies

--- .config/scripts/xdg_desktop_portal.sh
+++ .config/scripts/xdg_desktop_portal.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-sleep 1
+sleep 4
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-gtk
 killall xdg-desktop-portal
-# /usr/lib/xdg-desktop-portal-hyprland &
-systemctl --user start xdg-desktop-portal-hyprland.service
-sleep 2
-systemctl --user start xdg-desktop-portal.service
-# /usr/lib/xdg-desktop-portal &
+sleep 4
+systemctl --user start xdg-desktop-portal.service &
+systemctl --user start xdg-desktop-portal-gtk.service &
+systemctl --user start xdg-desktop-portal-hyprland.service &
