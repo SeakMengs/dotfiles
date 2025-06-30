c62cb76487d9f81d841259cda075f8e81d11e9ef
SeakMengs <hseakmeng22@gmail.com>
2025-06-30 14:52:43 +0700

    Fix screenshot include half cursor on bottom right

--- .config/scripts/region_screenshot.sh
+++ .config/scripts/region_screenshot.sh
@@ -1,38 +1,23 @@
 #!/bin/bash
 
-# Check if grim and slurp are installed
-if ! command -v grim &>/dev/null || ! command -v slurp &>/dev/null; then
-  echo "Error: grim or slurp is not installed."
-  notify-send "Error" "grim or slurp is not installed."
+# Check if grimblast is installed
+if ! command -v grimblast &>/dev/null; then
+  notify-send "Error" "grimblast is not installed."
   exit 1
 fi
 
-# Screenshot save path
 SAVE_DIR="$HOME/Pictures/Screenshots"
 
-# Check if the save path exists, if not, create it
-if [ ! -d "$SAVE_DIR" ]; then
-  mkdir -p "$SAVE_DIR"
-fi
+mkdir -p "$SAVE_DIR"
 
-# Select a region and capture its geometry | Uncomment this if using grim
-# region=$(slurp)
-# if [ -z "$region" ]; then
-#   echo "Screenshot canceled or failed."
-#   notify-send "Screenshot" "Screenshot canceled or failed."
-#   exit 1
-# fi
+FILENAME="Screenshot-$(date +%F_%T).png"
+SAVE_PATH="$SAVE_DIR/$FILENAME"
 
-# Take a screenshot of the selected region
-# grim -g "$region" - | wl-copy
+# Take a screenshot of a region (area) and save+copy it
+grimblast --freeze copysave area "$SAVE_PATH"
 
-# Save the screenshot from clipboard
-# wl-paste >"$SAVE_DIR/Screenshot-$(date +%F_%T).png"
-# notify-send "Screenshot" "Screenshot of the region taken"
-
-grimblast --freeze copysave area "$SAVE_DIR/Screenshot-$(date +%F_%T).png"
-
-#  Check the exit status of grimblast to determine if the operation was successful
 if [ $? -eq 0 ]; then
-  notify-send "Screenshot Taken" "Screenshot saved to $SAVE_DIR"
+  notify-send "Screenshot Taken" "Saved to $SAVE_PATH"
+else
+  notify-send "Screenshot Failed" "Could not take screenshot"
 fi
