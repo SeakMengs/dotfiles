216c865ec185b2dbc77c6634cfcbed2c32f441e7
SeakMengs <hseakmeng22@gmail.com>
2025-08-08 18:08:02 +0700

    bump nvim depedencies

--- .config/nvim/lua/plugins/nvim-lsp.lua
+++ .config/nvim/lua/plugins/nvim-lsp.lua
@@ -2,6 +2,23 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {},
+    opts = {
+      -- @type lspconfig.options
+      servers = {
+        phpactor = false,
+        intelephense = {
+          filetypes = { "php", "blade", "php_only" },
+          settings = {
+            intelephense = {
+              filetypes = { "php", "blade", "php_only" },
+              files = {
+                associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+                maxSize = 5000000,
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,9 +4,7 @@ return {
   ---@type snacks.Config
   opts = {
     explorer = {
-      -- your explorer configuration comes here
       -- or leave it empty to use the default settings
-      -- refer to the configuration section below
     },
     picker = {
       layout = {
@@ -14,26 +12,27 @@ return {
         preset = "telescope",
       },
       sources = {
-        explorer = {
-          auto_close = true,
-          layout = {
-            { preview = true },
-            layout = {
-              box = "horizontal", -- arrange horizontally (list left, preview right)
-              width = 0.9,
-              height = 0.8,
-              {
-                box = "vertical",
-                border = "rounded",
-                title = "{source} {live} {flags}",
-                title_pos = "center",
-                { win = "input", height = 1, border = "bottom" },
-                { win = "list", border = "none" },
-              },
-              { win = "preview", border = "rounded", width = 0.7, title = "{preview}" },
-            },
-          },
-        },
+        -- If prefer floating similar to oil.nvim, use this instead
+        -- explorer = {
+        --   auto_close = true,
+        --   layout = {
+        --     { preview = true },
+        --     layout = {
+        --       box = "horizontal", -- arrange horizontally (list left, preview right)
+        --       width = 0.9,
+        --       height = 0.8,
+        --       {
+        --         box = "vertical",
+        --         border = "rounded",
+        --         title = "{source} {live} {flags}",
+        --         title_pos = "center",
+        --         { win = "input", height = 1, border = "bottom" },
+        --         { win = "list", border = "none" },
+        --       },
+        --       { win = "preview", border = "rounded", width = 0.7, title = "{preview}" },
+        --     },
+        --   },
+        -- },
       },
     },
   },
